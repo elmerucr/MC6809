c05f8bd4def550b93f86d3c44bb72244a12d0236
elmerucr <elmerucr@gmail.com>
2023-12-16 19:18:53 +0100

    avoid macro _STD issue with windows

--- src/mc6809_disassembler.cpp
+++ src/mc6809_disassembler.cpp
@@ -14,23 +14,23 @@
 #include <cstdio>
 
 enum mnemonics_index {
-	_ABX=0,_ADCA, _ADCB, _ADDA, _ADDB, _ADDD, _ANDA, _ANDB,
-	_ANDCC,_ASL,  _ASLA, _ASLB, _ASR,  _ASRA, _ASRB, _BEQ,
-	_BGE,  _BGT,  _BHI,  _BHS,  _BITA, _BITB, _BMI,  _BLE,
-	_BLO,  _BLS,  _BLT,  _BNE,  _BPL,  _BRA,  _BRN,  _BSR,
-	_BVC,  _BVS,  _CLR,  _CLRA, _CLRB, _CMPA, _CMPB, _CMPD,
-	_CMPS, _CMPU, _CMPX, _CMPY, _COM,  _COMA, _COMB, _CWAI,
-	_DAA,  _DEC,  _DECA, _DECB, _EORA, _EORB, _EXG,  _ILL,
-	_INC,  _INCA, _INCB, _JMP,  _JSR,  _LBEQ, _LBGE, _LBGT,
-	_LBHI, _LBHS, _LBLE, _LBLO, _LBLS, _LBLT, _LBMI, _LBNE,
-	_LBPL, _LBRA, _LBRN, _LBSR, _LBVC, _LBVS, _LDA,  _LDB,
-	_LDD,  _LDS,  _LDU,  _LDX,  _LDY,  _LEAS, _LEAU, _LEAX,
-	_LEAY, _LSR,  _LSRA, _LSRB, _MUL,  _NEG,  _NEGA, _NEGB,
-	_NOP,  _ORA,  _ORB,  _ORCC, _PSHS, _PSHU, _PULS, _PULU,
-	_ROL,  _ROLA, _ROLB, _ROR,  _RORA, _RORB, _RTI,  _RTS,
-	_SBCA, _SBCB, _SEX,  _STA,  _STB,  _STD,  _STS,  _STU,
-	_STX,  _STY,  _SUBA, _SUBB, _SUBD, _SWI,  _SWI2, _SWI3,
-	_SYNC, _TFR,  _TST,  _TSTA, _TSTB
+	__ABX=0,__ADCA, __ADCB, __ADDA, __ADDB, __ADDD, __ANDA, __ANDB,
+	__ANDCC,__ASL,  __ASLA, __ASLB, __ASR,  __ASRA, __ASRB, __BEQ,
+	__BGE,  __BGT,  __BHI,  __BHS,  __BITA, __BITB, __BMI,  __BLE,
+	__BLO,  __BLS,  __BLT,  __BNE,  __BPL,  __BRA,  __BRN,  __BSR,
+	__BVC,  __BVS,  __CLR,  __CLRA, __CLRB, __CMPA, __CMPB, __CMPD,
+	__CMPS, __CMPU, __CMPX, __CMPY, __COM,  __COMA, __COMB, __CWAI,
+	__DAA,  __DEC,  __DECA, __DECB, __EORA, __EORB, __EXG,  __ILL,
+	__INC,  __INCA, __INCB, __JMP,  __JSR,  __LBEQ, __LBGE, __LBGT,
+	__LBHI, __LBHS, __LBLE, __LBLO, __LBLS, __LBLT, __LBMI, __LBNE,
+	__LBPL, __LBRA, __LBRN, __LBSR, __LBVC, __LBVS, __LDA,  __LDB,
+	__LDD,  __LDS,  __LDU,  __LDX,  __LDY,  __LEAS, __LEAU, __LEAX,
+	__LEAY, __LSR,  __LSRA, __LSRB, __MUL,  __NEG,  __NEGA, __NEGB,
+	__NOP,  __ORA,  __ORB,  __ORCC, __PSHS, __PSHU, __PULS, __PULU,
+	__ROL,  __ROLA, __ROLB, __ROR,  __RORA, __RORB, __RTI,  __RTS,
+	__SBCA, __SBCB, __SEX,  __STA,  __STB,  __STD,  __STS,  __STU,
+	__STX,  __STY,  __SUBA, __SUBB, __SUBD, __SWI,  __SWI2, __SWI3,
+	__SYNC, __TFR,  __TST,  __TSTA, __TSTB
 };
 
 enum addr_mode_index {
@@ -106,108 +106,108 @@ const struct exg_tfr_operand exg_tfr_operands[16] = {
 };
 
 enum mnemonics_index opcodes_page_1[256] = {
-	_NEG,	_ILL,	_ILL,	_COM,	_LSR,	_ILL,	_ROR,	_ASR,	// 0x00
-	_ASL,	_ROL,	_DEC,	_ILL,	_INC,	_TST,	_JMP,	_CLR,
-	_ILL,	_ILL,	_NOP,	_SYNC,	_ILL,	_ILL,	_LBRA,	_LBSR,	// 0x10
-	_ILL,	_DAA,	_ORCC,	_ILL,	_ANDCC,	_SEX,	_EXG,	_TFR,
-	_BRA,	_BRN,	_BHI,	_BLS,	_BHS,	_BLO,	_BNE,	_BEQ,	// 0x20
-	_BVC,	_BVS,	_BPL,	_BMI,	_BGE,	_BLT,	_BGT,	_BLE,
-	_LEAX,	_LEAY,	_LEAS,	_LEAU,	_PSHS,	_PULS,	_PSHU,	_PULU,	// 0x30
-	_ILL,	_RTS,	_ABX,	_RTI,	_CWAI,	_MUL,	_ILL,	_SWI,
-	_NEGA,	_ILL,	_ILL,	_COMA,	_LSRA,	_ILL,	_RORA,	_ASRA,	// 0x40
-	_ASLA,	_ROLA,	_DECA,	_ILL,	_INCA,	_TSTA,	_ILL,	_CLRA,
-	_NEGB,	_ILL,	_ILL,	_COMB,	_LSRB,	_ILL,	_RORB,	_ASRB,	// 0x50
-	_ASLB,	_ROLB,	_DECB,	_ILL,	_INCB,	_TSTB,	_ILL,	_CLRB,
-	_NEG,	_ILL,	_ILL,	_COM,	_LSR,	_ILL,	_ROR,	_ASR,	// 0x60
-	_ASL,	_ROL,	_DEC,	_ILL,	_INC,	_TST,	_JMP,	_CLR,
-	_NEG,	_ILL,	_ILL,	_COM,	_LSR,	_ILL,	_ROR,	_ASR,	// 0x70
-	_ASL,	_ROL,	_DEC,	_ILL,	_INC,	_TST,	_JMP,	_CLR,
-	_SUBA,	_CMPA,	_SBCA,	_SUBD,	_ANDA,	_BITA,	_LDA,	_ILL,	// 0x80
-	_EORA,	_ADCA,	_ORA,	_ADDA,	_CMPX,	_BSR,	_LDX,	_ILL,
-	_SUBA,	_CMPA,	_SBCA,	_SUBD,	_ANDA,	_BITA,	_LDA,	_STA,	// 0x90
-	_EORA,	_ADCA,	_ORA,	_ADDA,	_CMPX,	_JSR,	_LDX,	_STX,
-	_SUBA,	_CMPA,	_SBCA,	_SUBD,	_ANDA,	_BITA,	_LDA,	_STA,	// 0xa0
-	_EORA,	_ADCA,	_ORA,	_ADDA,	_CMPX,	_JSR,	_LDX,	_STX,
-	_SUBA,	_CMPA,	_SBCA,	_SUBD,	_ANDA,	_BITA,	_LDA,	_STA,	// 0xb0
-	_EORA,	_ADCA,	_ORA,	_ADDA,	_CMPX,	_JSR,	_LDX,	_STX,
-	_SUBB,	_CMPB,	_SBCB,	_ADDD,	_ANDB,	_BITB,	_LDB,	_ILL,	// 0xc0
-	_EORB,	_ADCB,	_ORB,	_ADDB,	_LDD,	_ILL,	_LDU,	_ILL,
-	_SUBB,	_CMPB,	_SBCB,	_ADDD,	_ANDB,	_BITB,	_LDB,	_STB,	// 0xd0
-	_EORB,	_ADCB,	_ORB,	_ADDB,	_LDD,	_STD,	_LDU,	_STU,
-	_SUBB,	_CMPB,	_SBCB,	_ADDD,	_ANDB,	_BITB,	_LDB,	_STB,	// 0xe0
-	_EORB,	_ADCB,	_ORB,	_ADDB,	_LDD,	_STD,	_LDU,	_STU,
-	_SUBB,	_CMPB,	_SBCB,	_ADDD,	_ANDB,	_BITB,	_LDB,	_STB,	// 0xf0
-	_EORB,	_ADCB,	_ORB,	_ADDB,	_LDD,	_STD,	_LDU,	_STU
+	__NEG,	__ILL,	__ILL,	__COM,	__LSR,	__ILL,	__ROR,	__ASR,	// 0x00
+	__ASL,	__ROL,	__DEC,	__ILL,	__INC,	__TST,	__JMP,	__CLR,
+	__ILL,	__ILL,	__NOP,	__SYNC,	__ILL,	__ILL,	__LBRA,	__LBSR,	// 0x10
+	__ILL,	__DAA,	__ORCC,	__ILL,	__ANDCC,__SEX,	__EXG,	__TFR,
+	__BRA,	__BRN,	__BHI,	__BLS,	__BHS,	__BLO,	__BNE,	__BEQ,	// 0x20
+	__BVC,	__BVS,	__BPL,	__BMI,	__BGE,	__BLT,	__BGT,	__BLE,
+	__LEAX,	__LEAY,	__LEAS,	__LEAU,	__PSHS,	__PULS,	__PSHU,	__PULU,	// 0x30
+	__ILL,	__RTS,	__ABX,	__RTI,	__CWAI,	__MUL,	__ILL,	__SWI,
+	__NEGA,	__ILL,	__ILL,	__COMA,	__LSRA,	__ILL,	__RORA,	__ASRA,	// 0x40
+	__ASLA,	__ROLA,	__DECA,	__ILL,	__INCA,	__TSTA,	__ILL,	__CLRA,
+	__NEGB,	__ILL,	__ILL,	__COMB,	__LSRB,	__ILL,	__RORB,	__ASRB,	// 0x50
+	__ASLB,	__ROLB,	__DECB,	__ILL,	__INCB,	__TSTB,	__ILL,	__CLRB,
+	__NEG,	__ILL,	__ILL,	__COM,	__LSR,	__ILL,	__ROR,	__ASR,	// 0x60
+	__ASL,	__ROL,	__DEC,	__ILL,	__INC,	__TST,	__JMP,	__CLR,
+	__NEG,	__ILL,	__ILL,	__COM,	__LSR,	__ILL,	__ROR,	__ASR,	// 0x70
+	__ASL,	__ROL,	__DEC,	__ILL,	__INC,	__TST,	__JMP,	__CLR,
+	__SUBA,	__CMPA,	__SBCA,	__SUBD,	__ANDA,	__BITA,	__LDA,	__ILL,	// 0x80
+	__EORA,	__ADCA,	__ORA,	__ADDA,	__CMPX,	__BSR,	__LDX,	__ILL,
+	__SUBA,	__CMPA,	__SBCA,	__SUBD,	__ANDA,	__BITA,	__LDA,	__STA,	// 0x90
+	__EORA,	__ADCA,	__ORA,	__ADDA,	__CMPX,	__JSR,	__LDX,	__STX,
+	__SUBA,	__CMPA,	__SBCA,	__SUBD,	__ANDA,	__BITA,	__LDA,	__STA,	// 0xa0
+	__EORA,	__ADCA,	__ORA,	__ADDA,	__CMPX,	__JSR,	__LDX,	__STX,
+	__SUBA,	__CMPA,	__SBCA,	__SUBD,	__ANDA,	__BITA,	__LDA,	__STA,	// 0xb0
+	__EORA,	__ADCA,	__ORA,	__ADDA,	__CMPX,	__JSR,	__LDX,	__STX,
+	__SUBB,	__CMPB,	__SBCB,	__ADDD,	__ANDB,	__BITB,	__LDB,	__ILL,	// 0xc0
+	__EORB,	__ADCB,	__ORB,	__ADDB,	__LDD,	__ILL,	__LDU,	__ILL,
+	__SUBB,	__CMPB,	__SBCB,	__ADDD,	__ANDB,	__BITB,	__LDB,	__STB,	// 0xd0
+	__EORB,	__ADCB,	__ORB,	__ADDB,	__LDD,	__STD,	__LDU,	__STU,
+	__SUBB,	__CMPB,	__SBCB,	__ADDD,	__ANDB,	__BITB,	__LDB,	__STB,	// 0xe0
+	__EORB,	__ADCB,	__ORB,	__ADDB,	__LDD,	__STD,	__LDU,	__STU,
+	__SUBB,	__CMPB,	__SBCB,	__ADDD,	__ANDB,	__BITB,	__LDB,	__STB,	// 0xf0
+	__EORB,	__ADCB,	__ORB,	__ADDB,	__LDD,	__STD,	__LDU,	__STU
 };
 
 enum mnemonics_index opcodes_page_2[256] = {
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x00
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x10
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_LBRN,	_LBHI,	_LBLS,	_LBHS,	_LBLO,	_LBNE,	_LBEQ,	// 0x20
-	_LBVC,	_LBVS,	_LBPL,	_LBMI,	_LBGE,	_LBLT,	_LBGT,	_LBLE,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x30
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_SWI2,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x40
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x50
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x60
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x70
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPD,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x80
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPY,	_ILL,	_LDY,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPD,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x90
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPY,	_ILL,	_LDY,	_STY,
-	_ILL,	_ILL,	_ILL,	_CMPD,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xa0
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPY,	_ILL,	_LDY,	_STY,
-	_ILL,	_ILL,	_ILL,	_CMPD,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xb0
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPY,	_ILL,	_LDY,	_STY,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xc0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_LDS,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xd0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_LDS,	_STS,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xe0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_LDS,	_STS,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xf0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_LDS,	_STS
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x00
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x10
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__LBRN,	__LBHI,	__LBLS,	__LBHS,	__LBLO,	__LBNE,	__LBEQ,	// 0x20
+	__LBVC,	__LBVS,	__LBPL,	__LBMI,	__LBGE,	__LBLT,	__LBGT,	__LBLE,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x30
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__SWI2,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x40
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x50
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x60
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x70
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPD,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x80
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPY,	__ILL,	__LDY,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPD,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x90
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPY,	__ILL,	__LDY,	__STY,
+	__ILL,	__ILL,	__ILL,	__CMPD,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xa0
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPY,	__ILL,	__LDY,	__STY,
+	__ILL,	__ILL,	__ILL,	__CMPD,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xb0
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPY,	__ILL,	__LDY,	__STY,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xc0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__LDS,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xd0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__LDS,	__STS,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xe0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__LDS,	__STS,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xf0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__LDS,	__STS
 };
 
 enum mnemonics_index opcodes_page_3[256] = {
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x00
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x10
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x20
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x30
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_SWI3,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x40
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x50
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x60
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x70
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPU,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x80
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPS,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPU,	_ILL,	_ILL,	_ILL,	_ILL,	// 0x90
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPS,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPU,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xa0
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPS,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_CMPU,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xb0
-	_ILL,	_ILL,	_ILL,	_ILL,	_CMPS,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xc0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xd0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xe0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	// 0xf0
-	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL,	_ILL
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x00
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x10
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x20
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x30
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__SWI3,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x40
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x50
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x60
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x70
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPU,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x80
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPS,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPU,	__ILL,	__ILL,	__ILL,	__ILL,	// 0x90
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPS,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPU,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xa0
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPS,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__CMPU,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xb0
+	__ILL,	__ILL,	__ILL,	__ILL,	__CMPS,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xc0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xd0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xe0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	// 0xf0
+	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL,	__ILL
 };
 
 enum addr_mode_index addr_mode_page_1[256] = {
